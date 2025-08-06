0cc849e72958e6aec97f187aad17fd12410815b1
shivang8077 <shivangjawla123@gmail.com>
2025-08-06 17:25:10 +0530

    Tailwind is Configured

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline text-blue-500'>
+        <p>hello </p>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () =>{
+    return (
+        <div>
+            <ul>
+                <li><img ></img></li>
+                <li>Home</li>
+                <li>Movies</li>
+                <li>Shows</li>
+
+            </ul>
+        </div>
+    )
+
+}
+export default Navbar;
